36a5057e9d4b3bc63d55be4eca8b9eb7674f4688
noxxgk <noxxgk@gmail.com>
2026-05-04 18:34:39 +0200

    Initial commit: Project setup

---
+++ src/main/java/com/gyms/memberships_manager/MembershipsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gyms.memberships_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyms/memberships_manager/MembershipsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyms.memberships_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
